66c0e04899bbf1f203e0c8aca3b3e1b6337d08c1
Junior <juniorquegu@gmail.com>
2024-11-21 23:49:59 -0500

    fix dummy import for tests

--- Sources/LyraPaymentSDKDependencies/Dummy.swift
+++ Sources/LyraPaymentSDKDependencies/Dummy.swift
@@ -10,6 +10,6 @@ import Sentry
 import SnapKit
 import Sentry
 import sentry_client_cocoa
-import Material
-import Motion
-import ios_cards_camera_recognizer
+import LyraMaterial
+import LyraMotion
+import LyraCardsRecognizer
